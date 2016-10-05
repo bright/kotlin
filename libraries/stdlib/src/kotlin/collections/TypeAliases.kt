fd438bddfa8d33b885565bc0ea3b5b7bdf1fd0c6
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-10-06 00:46:44 +0300

    ~ typealiases: rename type parameter to match type parameter of aliased class

--- libraries/stdlib/src/kotlin/collections/TypeAliases.kt
+++ libraries/stdlib/src/kotlin/collections/TypeAliases.kt
@@ -7,15 +7,15 @@ public typealias Comparator<T> = java.util.Comparator<T> // kotlin.comparisons?
 
 
 // internal?
-public typealias ArrayList<T> = java.util.ArrayList<T>
+public typealias ArrayList<E> = java.util.ArrayList<E>
 public typealias LinkedHashMap<K, V> = java.util.LinkedHashMap<K, V>
 public typealias HashMap<K, V> = java.util.HashMap<K, V>
-public typealias LinkedHashSet<T> = java.util.LinkedHashSet<T>
-public typealias HashSet<T> = java.util.HashSet<T>
+public typealias LinkedHashSet<E> = java.util.LinkedHashSet<E>
+public typealias HashSet<E> = java.util.HashSet<E>
 
 
 internal typealias Collections = java.util.Collections
 internal typealias Arrays = java.util.Arrays
 
-internal typealias SortedSet<T> = java.util.SortedSet<T>
-internal typealias TreeSet<T> = java.util.TreeSet<T>
+internal typealias SortedSet<E> = java.util.SortedSet<E>
+internal typealias TreeSet<E> = java.util.TreeSet<E>
